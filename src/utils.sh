d01f9e6d7871960303e331b126fa07f4ef623481
MJkhan1400 <mjkhan1400@gmail.com>
2025-11-16 20:50:22 +0400

    renamed the .tmux file and reverted the color changes

--- src/utils.sh
+++ src/utils.sh
@@ -24,7 +24,7 @@ function generate_left_side_string() {
 		local separator_end="#[bg=${PALLETE[bg_highlight]}]#{?client_prefix,#[fg=${PALLETE[yellow]}],#[fg=${PALLETE[green]}]}${left_separator:?}#[none]"
 	fi
 
-	echo "#[fg=${PALLETE[fg_gutter]},bold]#{?client_prefix,#[bg=${PALLETE[yellow]}],#[bg=${PALLETE[fg_dark]}]} ${session_icon} #S ${separator_end}"
+	echo "#[fg=${PALLETE[fg_gutter]},bold]#{?client_prefix,#[bg=${PALLETE[yellow]}],#[bg=${PALLETE[green]}]} ${session_icon} #S ${separator_end}"
 }
 
 function generate_inactive_window_string() {
@@ -47,7 +47,7 @@ function generate_inactive_window_string() {
 		local separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['dark3']}]${left_separator:?}#[none]"
 	fi
 
-	echo "${separator_start}#[fg=${PALLETE[fg]}]#I${separator_internal}#[fg=${PALLETE[fg]}] #{?window_zoomed_flag,$zoomed_window_icon,$inactive_window_icon}${inactive_window_title}${separator_end}"
+	echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$inactive_window_icon}${inactive_window_title}${separator_end}"
 }
 
 function generate_active_window_string() {
